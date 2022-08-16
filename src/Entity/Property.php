d0595cab005c4c8bb8f7867c44b72e0807403800
Dimitri RAMANANTSOA <dimitri.rama.pro@gmail.com>
2022-08-16 17:38:04 +0200

    add constraints on property entity

--- src/Entity/Property.php
+++ src/Entity/Property.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use Assert\NotNull;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\PropertyRepository;
@@ -9,11 +10,26 @@ use Doctrine\Common\Collections\Collection;
 use ApiPlatform\Core\Annotation\ApiResource;
 use Doctrine\Common\Collections\ArrayCollection;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Positive;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 #[ORM\Entity(repositoryClass: PropertyRepository::class)]
 #[ApiResource(
-    normalizationContext: ['groups' => ['property:read']]
+    normalizationContext: ['groups' => ['property:read']],
+    denormalizationContext: ['groups' => ['property:write']],
+    paginationItemsPerPage: 10,
+    paginationMaximumItemsPerPage: 100,
+    paginationClientItemsPerPage: true,
+    collectionOperations: [
+        'get',
+        'post'
+    ],
+    itemOperations: [
+        'get',
+        'patch',
+        'delete'
+    ],
 )]
 #[UniqueEntity('title')]
 class Property
@@ -25,51 +41,96 @@ class Property
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['media_object:read', 'property:read'])]
+    #[Groups(['media_object:read', 'property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Length(min: 5, max: 30)
+    ]
     private ?string $title = null;
 
     #[ORM\Column(type: Types::TEXT)]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Length(min: 5, max: 255)
+    ]
     private ?string $description = null;
 
     #[ORM\Column]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Range(min: 20, max: 400)
+    ]
     private ?int $surface = null;
 
     #[ORM\Column(type: Types::SMALLINT)]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Range(min: 1)
+    ]
     private ?int $room = null;
 
     #[ORM\Column(type: Types::SMALLINT)]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Range(min: 1)
+    ]
     private ?int $bedroom = null;
 
     #[ORM\Column(type: Types::SMALLINT)]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Positive
+    ]
     private ?int $floor = null;
 
     #[ORM\Column]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Positive
+    ]
     private ?int $price = null;
 
     #[ORM\Column(type: Types::SMALLINT)]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Positive
+    ]
     private ?int $heat = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Length(min: 2, max: 30)
+    ]
     private ?string $city = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Length(min: 5, max: 255)
+    ]
     private ?string $address = null;
 
     #[ORM\Column(length: 255)]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
+    #[
+        NotBlank,
+        Length(min: 5, max: 255),
+        Regex('/^[0-9]{5}/')
+    ]
     private ?string $postalcode = null;
 
     #[ORM\Column]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
     private ?bool $sold = false;
 
     #[ORM\Column(type: Types::DATETIME_MUTABLE)]
@@ -81,11 +142,11 @@ class Property
     private ?\DateTimeInterface $updatedAt = null;
 
     #[ORM\ManyToMany(targetEntity: Options::class, inversedBy: 'properties')]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
     private Collection $options;
 
     #[ORM\OneToMany(mappedBy: 'property', targetEntity: MediaObject::class, cascade: ["persist", "remove"])]
-    #[Groups(['property:read'])]
+    #[Groups(['property:read', 'property:write'])]
     private Collection $pictures;
 
     public function __construct()
